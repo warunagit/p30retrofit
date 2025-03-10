675fb4beac4c66ef805d5b60b158c6649454c793
warunagit <waruna.git@gmail.com>
2025-03-10 16:57:31 +0200

    commments added

--- app/src/main/java/com/cycolabs/p30retrofit/MainActivity.kt
+++ app/src/main/java/com/cycolabs/p30retrofit/MainActivity.kt
@@ -7,6 +7,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    //Web services:
+    //download-upload data, Authentication, Store images, Upload files...
+    //API: Appplication Programming Interface use for apllication's functionality and data communication
+    //JSON: Data will be transfer in JSON format. Should parse to required data type to use.
+    //JSON: JavaScript Object Notation. It is super lightweight.
+    //App -> Request with params -> RETROFIT -> Uri and HTTP method -> Server
+    //Server -> Response in JSON format -> RETROFIT -> Kotlin Object -> App
+    //RETFROFIT provides authentication and POJO parse conversion for "source" of object.
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
